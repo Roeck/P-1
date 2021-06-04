7244a538816c001da53cc66413cbbc4daf17ff82
Roeck <rons9n@gmail.com>
2021-06-03 22:04:24 -0400

    Users controller and app helper for Gravatar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  devise_for :users
+
+  devise_for :users,
+    path: '',
+    path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'}
 end
