3b5b56f8cafde6a7c9ca04c709e024254cd91393
Roeck <rons9n@gmail.com>
2021-06-06 03:47:24 -0400

    Logout fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   devise_for :users
+  
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
     # path: '',
-    # path_names {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'}
+    # path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
 
     resources :users, only: [:show]
 end
